55859d56a8073fe28c492640d675cc0b1363a4e4
xrfind <xrfind@gmail.com>
2013-07-15 14:15:08 +0800

    add ajax to voting link

--- theme/patternentity.js
+++ theme/patternentity.js
@@ -3,25 +3,33 @@
  * patternentity list table js.
  *
  */
-jQuery(document).ready(function() {
-  //when click on download link, download times grown by 1.
-  var download_link = jQuery(".pattern-entity-list-table .download-link");
-  download_link.click(function() {
-      var download_times = jQuery(this).parent().prev().prev();
-      var numb = Number(download_times.text()) + 1; 
-      download_times.text(numb);
-  });
+(function($){
+$(document).ready(function() {
+	//when click on download link, download times grown by 1.
+	var download_link = $(".pattern-entity-list-table .download-link");
+	download_link.click(function() {
+		var download_times = $(this).parent().prev().prev();
+		var numb = Number(download_times.text()) + 1; 
+		download_times.text(numb);
+	});
 
-  //var url_div = window.location.href;
-  //url_div += "#pattern-entity-list-table-id";
-  //alert(url_div);
+	//voting link ajax.
+	$('#pattern-entity-list-table-id .voting-link').click(function () {
+		 var link = this;
+		 var url = $(this).attr('href');
+		 //console.log(url);
+		 $.ajax({
+			url: url,
+			success: function(data) {
+				var $score = $('#patternentity-page-pattern-description dl dd:last', data);
+				//console.log($score.text());
+				//console.log($(link).text());
 
-  //var voting_link = jQuery(".pattern-entity-list-table .voting-link");
-  //voting_link.click(function() {
-  //    //var url_div = jQuery(this).attr("href");
-  //    //url_div += "#content";
-  //    //alert(url_div);
-  //    jQuery(document).load(url_div);
-  //    return false;
-  //});
+				$(link).parent().prev().prev().html($score.text());
+				//console.log(this);
+			}
+		});
+		return false;
+	});
 });
+})(jQuery)
